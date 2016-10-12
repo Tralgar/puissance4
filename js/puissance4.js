029dc7b200f914abeec49e72dd8bdbf475c9e521
nored91 <thoretton.norman@gmail.com>
2016-10-12 12:47:38 +0200

    avancement base

--- js/puissance4.js
+++ js/puissance4.js
@@ -1,29 +1,66 @@
 var Master = {
   init: function () {
+    console.log('Initialisation');
     // checker la diffultée
     $('#button-start').click(function () {
-      this.startGame();
+      Master.startGame();
     });
 
     $('#puissance4-actionRow th button').click(function () {
-      this.addToken();
+      Master.addToken();
     });
   },
   startGame: function () {
+    Master.displayMessage("Début de la partie");
 
+    var random = (Math.floor((Math.random() * 10) + 1) <= 5); 
+    if(random){
+      Master.humanPlay();
+    }else{
+      Master.iAPlay();
+    }
   },
-  addToken: function () {
+  addToken: function (numCol,b) {
+    //IA
+    if(b){
 
+    }
+    //Humain
+    else{
+
+    }
   },
   iAPlay: function() {
+    Master.displayMessage("Tour IA");
 
+    var listButton = $('#puissance4-actionRow button');
+    var max = $('#puissance4-actionRow button:not(:disabled)').length;
+
+    if($('#algo').val() == 0){
+      var nombreAleatoire = Math.floor(Math.random() * (max-1));
+    }
+    Master.refreshButton();
   },
   humanPlay: function() {
-
+    Master.displayMessage("Tour Humain");
   },
   checkEnd: function () {
 
+  },
+  displayMessage: function (str){
+    setTimeout(function() {
+      $('#message').html(str);
+    }, 500);
+  },
+  disableButton: function(){
+    //On désactive les boutons de base
+    $('#puissance4-actionRow button').attr('disabled','disabled');
+  },
+  refreshButton: function(){
+    //On désactive les boutons de base
+    $('#puissance4-actionRow button').removeAttr('disabled');
   }
 }
-
-Master.init();
+$(document).ready(function() {
+  Master.init();
+});
