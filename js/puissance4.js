efaf1409c109e7f84788a752ab5bea406bbcbc0f
Tralgar <leo.poupet@gmail.com>
2016-10-12 11:35:34 +0200

    le travail du mercredi matin

---
+++ js/puissance4.js
@@ -0,0 +1,29 @@
+var Master = {
+  init: function () {
+    // checker la diffultée
+    $('#button-start').click(function () {
+      this.startGame();
+    });
+
+    $('#puissance4-actionRow th button').click(function () {
+      this.addToken();
+    });
+  },
+  startGame: function () {
+
+  },
+  addToken: function () {
+
+  },
+  iAPlay: function() {
+
+  },
+  humanPlay: function() {
+
+  },
+  checkEnd: function () {
+
+  }
+}
+
+Master.init();
