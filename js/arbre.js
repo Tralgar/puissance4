8d9ddf2719631d382095aa71f3b8c0f179601887
Tralgar <leo.poupet@gmail.com>
2016-11-17 17:59:26 +0100

    minmax1 leo

--- js/arbre.js
+++ js/arbre.js
@@ -1,21 +1,29 @@
-const NIVEAUARBRE = 4;
-
-function Arbre(aFils){
+function Arbre(aFils) {
   this.fils = aFils;
 }
 
-
-function Noeud(){
-  this.colonne = null;
+function Noeud(colonne) {
+  this.colonne = colonne;
   this.valeur = null;
-  this.fils = null;
-}
-function Noeud(numColonne,valeur,aFils){
-  this.colonne = numColonne;
-  this.valeur = valeur;
-  this.fils = aFils;
+  this.fils = [];
 }
 
-function setFils(noeud,aFils){
-  noeud.fils = aFils;
+Noeud.prototype.setFils = function (aFils) {
+  this.fils = aFils;
+};
+
+Noeud.prototype.setValeur = function (valeur) {
+  this.valeur = valeur;
+};
+
+Noeud.prototype.toString = function () {
+  console.log("Je suis le noeud [" + this.colonne + ";" + this.valeur + "] et mes fils sont :");
+  this.fils.forEach(function (element) {
+    console.log("Fils : [" + element.colonne + ";" + element.valeur + "] ");
+  });
+}
+
+function Feuille(colonne, valeur) {
+  this.colonne = colonne;
+  this.valeur = valeur;
 }
